1397c9802936c307ce4458baad139899f091f358
Pradeep Bisht <129987749+Pradeepbisht1@users.noreply.github.com>
2024-10-17 14:14:42 +0530

    Update popular.ts

--- pages/api/popular.ts
+++ pages/api/popular.ts
@@ -1,18 +1,20 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-import axios from "../../utils/axios";
-
-const apiKey = process.env.OMDB_KEY;  // OMDb API Key
+import type { NextApiRequest, NextApiResponse } from 'next';
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  try {
-    const result = await axios.get('/', {
-      params: {
-        apikey: apiKey,   // OMDb uses 'apikey'
-        s: 'popular',     // Search term 'popular' (adjust according to OMDb behavior)
-      },
-    });
-    res.status(200).json(result.data);
-  } catch (error) {
-    res.status(500).json({ error: 'Failed to fetch popular movies from OMDb' });
-  }
+    const { title, type = '', year, page = 1 } = req.query;
+    const apiKey = '29efbf52';
+    const url = `http://www.omdbapi.com/?apikey=${apiKey}&s=${title}&type=${type}&y=${year}&page=${page}`;
+
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+
+        if (data.Response === 'True') {
+            res.status(200).json(data.Search);
+        } else {
+            res.status(404).json({ error: data.Error });
+        }
+    } catch (error) {
+        res.status(500).json({ error: 'An error occurred while fetching data' });
+    }
 };
